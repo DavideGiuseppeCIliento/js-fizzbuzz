00826c004cecf675d64fe5b6639c460c232663c6
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-04-02 17:28:54 +0200

    + esercizio Farfallino

--- js/script.js
+++ js/script.js
@@ -10,5 +10,30 @@
 //   }
 // }
 
-for (let i = 1; i <= 100; i++)
-  i % 3 === 0 && i % 5 === 0 ? console.log("FizzBuzz") : i % 3 === 0 ? console.log("Fizz") : i % 5 === 0 ? console.log("Buzz") : console.log(i);
+// for (let i = 1; i <= 100; i++)
+//   i % 3 === 0 && i % 5 === 0 ? console.log("FizzBuzz") : i % 3 === 0 ? console.log("Fizz") : i % 5 === 0 ? console.log("Buzz") : console.log(i);
+
+for (let i = 1; i <= 100; i++) console.log(i % 3 === 0 && i % 5 === 0 ? "FizzBuzz" : i % 3 === 0 ? "Fizz" : i % 5 === 0 ? "Buzz" : i);
+
+// const test = condizione ? valore1 : valore2;
+// const test = condizione1 ? valore1: (condizione2 ? valore2 : valore3);
+
+//FARFALLINO
+
+const stringa = prompt("Inserisci una parola");
+let finalStringa;
+
+for (let i = 0; i <= stringa.length; i++) {
+  stringa[i] === "a"
+    ? (finalStringa += "afa")
+    : stringa[i] === "e"
+    ? (finalStringa += "efe")
+    : stringa[i] === "i"
+    ? (finalStringa += "ifi")
+    : stringa[i] === "o"
+    ? (finalStringa += "ofo")
+    : stringa[i] === "u"
+    ? (finalStringa += "ufu")
+    : (finalStringa += stringa[i]);
+}
+alert(finalStringa);
